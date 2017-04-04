f161080ff96516885afa64fcb0a5c18458371e78
Rigo Ramirez <ram_ri@outlook.com>
2017-04-04 12:03:35 -0500

    added method to save the push notifications :bell:

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("resume", this.onResume, false);
     },
     // deviceready Event Handler
     //
@@ -36,6 +37,24 @@ var app = {
         console.log('Received Device Ready Event');
         console.log('calling setup push');
         app.setupPush();
+        app.DrawNotifications();
+        app.TestAppend();
+    },
+    DrawNotifications: function(){
+        if (localStorage.getItem("last") != null){
+            var last = parseInt(localStorage.getItem("last"));
+            var page = $(".page-content");
+            
+            for(var i = 0; i <= last; i++){
+                var data = localStorage.getItem("notif" + i);
+                data = JSON.parse(data);
+
+                page.append('<div class="card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__title">'
+                            + '<h2 class="mdl-card__title-text">' + data.title + '</h2></div>'
+                            + '<div class="mdl-card__supporting-text">' + data.message + '</div></div>');
+                page.trigger("create");
+            }
+        }
     },
     setupPush: function() {
         console.log('calling push init');
@@ -88,6 +107,13 @@ var app = {
                 'Ok'                  // buttonName
             );
 
+            navigator.notification.alert(
+                localStorage.getItem("last") == null,
+                null,
+                "Testing localStorage",
+                'Ok'
+            );
+
             if (localStorage.getItem("last") == null){
                 localStorage.setItem("last", "0");
             }
@@ -99,6 +125,26 @@ var app = {
             var last = parseInt(localStorage.getItem("last"));
             localStorage.setItem("notif" + last, JSON.stringify(data));
        });
+    },
+    onResume: function(){
+        var last = localStorage.getItem("last");
+        console.log(typeof(last));
+        console.log(last == null);
+        console.log(last);
+        // Show Snackbar
+        var notification = document.querySelector('.mdl-js-snackbar');
+        notification.MaterialSnackbar.showSnackbar(
+        {
+            message: last
+        });
+
+        app.DrawNotifications();
+    },
+    TestAppend: function(){
+        console.log("Test Append - - - - - - ");
+        var page = $(".page-content");
+        page.append("<p>It works</p>");
+        page.trigger("create");
     }
 };
 
